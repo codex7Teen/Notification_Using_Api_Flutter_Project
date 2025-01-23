407cc049b4f5a36833d5f070b0b016456839e7ce
Dennizx <djconnect189@gmail.com>
2025-01-23 20:06:57 +0530

    adjusted dimensions

--- lib/features/notification/presentation/screens/notification_screen.dart
+++ lib/features/notification/presentation/screens/notification_screen.dart
@@ -16,72 +16,76 @@ class ScreenNotifications extends StatelessWidget {
         backgroundColor: AppColors.whiteColor,
         //! APPBAR
         appBar: NotificationScreenWidgets.buildAppBar(context),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 20),
-          child: BlocBuilder<NotificationBloc, NotificationState>(
-            builder: (context, state) {
-              if (state is NotificationLoadingState) {
-                return Center(
-                    child: CircularProgressIndicator(
-                        color: AppColors.greenAppthemeColor));
-              } else if (state is NotificationErrorState) {
-                return Center(child: Text('Failed to load notifications!!!'));
-              } else if (state is NotificationLoadedState) {
-                final notificaitonModel = state.notifications;
-                return ListView.separated(
+        body: BlocBuilder<NotificationBloc, NotificationState>(
+          builder: (context, state) {
+            if (state is NotificationLoadingState) {
+              return Center(
+                  child: CircularProgressIndicator(
+                      color: AppColors.greenAppthemeColor));
+            } else if (state is NotificationErrorState) {
+              return Center(child: Text('Failed to load notifications!!!'));
+            } else if (state is NotificationLoadedState) {
+              final notificaitonModel = state.notifications;
+              return Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: ListView.separated(
                   itemCount: state.notifications.length,
                   separatorBuilder: (context, index) {
                     return Divider();
                   },
                   itemBuilder: (context, index) {
                     final notifications = notificaitonModel[index];
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                            height: 30,
-                            width: 30,
-                            child: Image.asset(context
-                                .read<NotificationBloc>()
-                                .getNotificationImagePath(
-                                    notifications.image))),
-                        SizedBox(width: 18),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
+                    return Padding(
+                      padding: const EdgeInsets.only(
+                          left: 18, right: 18, bottom: 3, top: 4),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                              height: 34,
+                              width: 34,
+                              child: Image.asset(context
+                                  .read<NotificationBloc>()
+                                  .getNotificationImagePath(
+                                      notifications.image))),
+                          SizedBox(width: 18),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                    overflow: TextOverflow.ellipsis,
+                                    notifications.title,
+                                    style: AppTextStyles.notificationTitle),
+                                SizedBox(height: 6),
+                                Text(
+                                  maxLines: 3,
                                   overflow: TextOverflow.ellipsis,
-                                  notifications.title,
-                                  style: AppTextStyles.notificationTitle),
-                              SizedBox(height: 6),
-                              Text(
-                                maxLines: 3,
-                                overflow: TextOverflow.ellipsis,
-                                notifications.body,
-                                style: AppTextStyles.notificationSubTitle,
-                              ),
-                              SizedBox(height: 4),
-                              Text(
-                                overflow: TextOverflow.ellipsis,
-                                context
-                                    .read<NotificationBloc>()
-                                    .formatTimestamp(notifications.timestamp),
-                                style: AppTextStyles.notificationTimeText,
-                              )
-                            ],
-                          ),
-                        )
-                      ],
+                                  notifications.body,
+                                  style: AppTextStyles.notificationSubTitle,
+                                ),
+                                SizedBox(height: 4),
+                                Text(
+                                  overflow: TextOverflow.ellipsis,
+                                  context
+                                      .read<NotificationBloc>()
+                                      .formatTimestamp(notifications.timestamp),
+                                  style: AppTextStyles.notificationTimeText,
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     );
                   },
-                );
-              }
-              return Text('Something went wrong! Please retry...');
-            },
-          ),
+                ),
+              );
+            }
+            return Text('Something went wrong! Please retry...');
+          },
         ));
   }
 }
